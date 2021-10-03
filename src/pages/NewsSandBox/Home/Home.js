1358138d0708b94b6017aa7bcd6183b4ec4e8628
xiaolin <xiaolin201809@126.com>
2021-10-04 01:20:49 +0800

    使用 json-server 实现增删改查

--- src/pages/NewsSandBox/Home/Home.js
+++ src/pages/NewsSandBox/Home/Home.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { Button } from "antd"
+import axios from 'axios'
 export default function index() {
+
+    const getData = () => {
+        // // 获取数据
+        // axios('http://localhost:8888/ports/1').then(res=>{
+        //     console.log(res.data)
+        // })
+
+        // // 增
+        // axios.post('http://localhost:8888/ports', {
+        //     title: '3333333',
+        //     author: '隔壁老王'
+        // })
+
+        // // 删除
+        // axios.delete('http://localhost:8888/ports/3')
+
+        // 改
+        axios.put('http://localhost:8888/ports/1', {
+            title: '3333333',
+            author: '隔壁老王'
+        })
+    }
+
     return (
         <div>
-            Home <Button type="primary">点击</Button>
+            Home <Button type="primary" onClick={getData}>点击</Button>
         </div>
     )
 }
