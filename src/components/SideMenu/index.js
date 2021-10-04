2f15ca480e5a084796ef7d8aa70d90c8d27a6aee
xiaolin <xiaolin201809@126.com>
2021-10-05 01:50:28 +0800

    根据后端返回的数据渲染导航

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Layout, Menu } from "antd"
 import { withRouter } from "react-router-dom"
 import {
@@ -6,61 +6,60 @@ import {
 } from '@ant-design/icons';
 
 import style from "./index.module.less"
+import axios from 'axios';
 
 const { Sider } = Layout
 const { SubMenu } = Menu
 const MenuItem = Menu.Item
 
-// 模拟导航数据
-const menuList = [
-  {
-    key: "/home",
-    title: "首页",
-    icon: <UserOutlined />
-  }, {
-    key: "/user-manage",
-    title: "用户管理",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "/user-manage/list",
-        title: "用户列表",
-        icon: <UserOutlined />
-      }
-    ]
-  }, {
-    key: "/right-manage",
-    title: "权限管理",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "/right-manage/role/list",
-        title: "角色列表",
-        icon: <UserOutlined />
-      }, {
-        key: "/right-manage/right/list",
-        title: "权限列表",
-        icon: <UserOutlined />
-      }
-    ]
+const iconList = {
+  "/home": <UserOutlined />,
+  "/user-manage": <UserOutlined />,
+  "/user-manage/list": <UserOutlined />,
+  "/right-manage": <UserOutlined />,
+  "/right-manage/role/list": <UserOutlined />,
+  "/right-manage/right/list": <UserOutlined />,
+  "/news-manage": <UserOutlined />,
+  "/news-manage/add": <UserOutlined />,
+  "/news-manage/draft": <UserOutlined />,
+  "/audit-manage": <UserOutlined />,
+  "/audit-manage/audit": <UserOutlined />,
+  "/audit-manage/list": <UserOutlined />,
+  "/publish-manage": <UserOutlined />,
+  "/publish-manage/unpublished": <UserOutlined />,
+  "/publish-manage/published": <UserOutlined />,
+  "/publish-manage/published": <UserOutlined />,
+  "/publish-manage/sunset": <UserOutlined />
+}
+
+
+function SideMenu(props) {
+
+  const [menu, setMenu] = useState([])
+
+  // 动态获取路由结构
+  useEffect(() => {
+    axios.get('http://localhost:8888/rights?_embed=children').then(res => {
+      setMenu(res.data)
+    })
+  }, [])
+
+  // 渲染判断 pagepermisson 是根据后端返回的判断页面路由的标识
+  const checkPagePermission = (item) => {
+    return item.pagepermisson === 1
   }
-]
-
-function SideMenu(props) {  
-
-  const routerIndex = props.history.location.pathname;
 
   // 动态生成导航
   const renderMenu = (menuList) => {
     return menuList && menuList.map(item => {
-      if (item.children) {
+      if (item.children && checkPagePermission(item)) {
         return (
-          <SubMenu key={item.key} title={item.title} icon={item.icon}>
+          <SubMenu key={item.key} title={item.title} icon={iconList[item.key]}>
             {renderMenu(item.children)}
           </SubMenu>
         )
       }
-      return (<MenuItem key={item.key} icon={item.icon} onClick={() => {
+      return checkPagePermission(item) && (<MenuItem key={item.key} icon={iconList[item.key]} onClick={() => {
         props.history.push(item.key)
       }}>{item.title}</MenuItem>)
     })
@@ -69,8 +68,8 @@ function SideMenu(props) {
   return (
     <Sider trigger={null} collapsible collapsed={false} className={style.sider} >
       <div className={style.logo}>全球新闻管理系统</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={[routerIndex]}>
-        {renderMenu(menuList)}
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+        {renderMenu(menu)}
       </Menu>
     </Sider>
   )
