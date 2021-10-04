9ec42d038f958ff38870af5e5163e0c93241fe30
xiaolin <xiaolin201809@126.com>
2021-10-05 01:21:01 +0800

    添加多表关联查询

--- src/pages/NewsSandBox/Home/Home.js
+++ src/pages/NewsSandBox/Home/Home.js
@@ -16,7 +16,12 @@ export default function Home() {
         // })
 
         // // 删除
-        // axios.delete('http://localhost:8888/ports/3')
+        // 在设置数据结构的时候，删除指定 id 的数据的时候，会删除关联的数据
+        // "ports": [{id:1, title:"1111111111", author: "小林"}]
+        // "comments": [{id:2, body:"12321321321-common", portId: 1}]
+        // 在删除 /ports/1 的时候，会连带删除 /comments/2 这条数据，因为这条数据的 portId = 1
+
+        // axios.delete('http://localhost:8888/ports/1')
 
         // // 改
         // axios.put('http://localhost:8888/ports/1', {
@@ -27,6 +32,16 @@ export default function Home() {
         // // 局部更新
         // axios.patch('http://localhost:8888/ports/2', {
         //     title:"222232323232"
+        // })
+
+        // // _embed 多表关联 根据文章获取到文章对应的评论
+        // axios.get('http://localhost:8888/ports?_embed=comments').then(res=>{
+        //     console.log(res.data)
+        // })
+
+        // // _expand 多表关联 根据评论获取到对应的文章
+        // axios.get('http://localhost:8888/comments?_expand=port').then(res => {
+        //     console.log(res)
         // })
     }
 
