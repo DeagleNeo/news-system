16d6c630121183e68e61334fc351cbc6f408fa58
xiaolin <xiaolin201809@126.com>
2021-10-04 22:40:20 +0800

    删除不必要的代码

--- src/pages/NewsSandBox/Home/Home.js
+++ src/pages/NewsSandBox/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from "antd"
 import axios from 'axios'
-export default function index() {
+export default function Home() {
 
     const getData = () => {
         // // 获取数据
@@ -18,11 +18,11 @@ export default function index() {
         // // 删除
         // axios.delete('http://localhost:8888/ports/3')
 
-        // 改
-        axios.put('http://localhost:8888/ports/1', {
-            title: '3333333',
-            author: '隔壁老王'
-        })
+        // // 改
+        // axios.put('http://localhost:8888/ports/1', {
+        //     title: '3333333',
+        //     author: '隔壁老王'
+        // })
     }
 
     return (
